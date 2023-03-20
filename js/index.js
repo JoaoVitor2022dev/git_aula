b2c97aa2acbef88763ea61b291e6a920da9f68ec
joao vitor <vl351962@gmail.com>
2023-03-20 14:55:09 -0400

    modificaçoes conscluidas

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const index = () => {
+ 
+    const [name, setName]= useState();
+
   return (
-    <div>index</div>
+    <div>
+        <h1>pagina principal {name} </h1>
+    </div>
   )
 }
 
